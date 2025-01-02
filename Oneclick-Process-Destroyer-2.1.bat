f632c33e39624561e5e3ecc826db2d1b0dceb732
cccqu <117171678+cccqu@users.noreply.github.com>
2025-01-02 07:01:11 -0300

    Update Oneclick-Process-Destroyer-2.1.bat

--- Oneclick-Process-Destroyer-2.1.bat
+++ Oneclick-Process-Destroyer-2.1.bat
@@ -32,11 +32,6 @@ reg.exe add "HKLM\%HIVE%\Services\gpsvc" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\EventLog" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\PlugPlay" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
-sc config SystemEventsBroker start=auto >nul 2>&1
-sc config Schedule start=auto >nul 2>&1
-sc config TimeBroker start=auto >nul 2>&1
-sc config TimeBrokerSvc start=auto >nul 2>&1
-sc config WSearch start=auto >nul 2>&1
 echo Closing in 3 seconds...
 timeout 3 > nul
 exit
