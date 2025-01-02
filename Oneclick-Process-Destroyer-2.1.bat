b42f4b430cdbf74b4147fbc7db37083c60578d2b
cccqu <117171678+cccqu@users.noreply.github.com>
2025-01-02 12:25:17 +1100

    Update Oneclick-Process-Destroyer-2.1.bat

--- Oneclick-Process-Destroyer-2.1.bat
+++ Oneclick-Process-Destroyer-2.1.bat
@@ -34,7 +34,6 @@ reg.exe add "HKLM\%HIVE%\Services\gpsvc" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\EventLog" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\PlugPlay" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
-reg.exe add "HKLM\%HIVE%\Services\msiserver" /v "Start" /t REG_DWORD /d "4" /f
 echo Closing in 3 seconds...
 timeout 3 > nul
 exit
